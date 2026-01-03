967c34ae409e295ae86397f0a66ff5358622fbbc
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2026-01-03 16:31:04 +0530

    Day02

---
+++ Day02/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package Day02;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
---
+++ Day02/RansomNote.java
@@ -0,0 +1,14 @@
+package Day02;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray())
+            freq[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            if (--freq[c - 'a'] < 0)
+                return false;
+        }
+        return true;
+    }
+}
