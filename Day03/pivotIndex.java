8acfc0f73dba323ef1af8c0c192f0cdb0304703e
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2026-01-08 17:13:15 +0530

    Day 03

---
+++ Day03/pivotIndex.java
@@ -0,0 +1,19 @@
+package Day03;
+
+public class pivotIndex {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += nums[i];
+        }
+        int lSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (lSum == totalSum - nums[i] - lSum) {
+                return i;
+            }
+            lSum += nums[i];
+        }
+        return -1;
+    }
+}
