4da6ab0d7a6d0bd3fe78bf152cf6791a155a0bc0
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2026-02-04 13:57:06 +0530

    Day25

---
+++ NextGreaterElementI.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int[] ans = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < n; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return ans;
+    }
+}
